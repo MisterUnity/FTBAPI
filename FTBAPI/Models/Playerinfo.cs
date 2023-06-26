91561730a785d9bebe86b5e7b4461e6317e05446
AllenHuang <korbat4625@gmail.com>
2023-06-26 23:00:47 +0800

    新增驗證機制範例

--- FTBAPI/Models/Playerinfo.cs
+++ FTBAPI/Models/Playerinfo.cs
@@ -1,29 +1,32 @@
-﻿using System;
+﻿using FTBAPI.Validations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FTBAPI.Models;
 
 public partial class Playerinfo
 {
     public Guid Id { get; set; }
-
+    [NonSpace, MaxLength(10)]
     public string Name { get; set; } = null!;
-
+    [NonSpace, MaxLength(1)]
     public string Gender { get; set; } = null!;
-
+    [NonSpace, MaxLength(10)]
     public string Height { get; set; } = null!;
-
+    [NonSpace, MaxLength(10)]
     public string Weight { get; set; } = null!;
-
+    [NonSpace, MaxLength(10)]
     public string? Position { get; set; }
-
+    [NonSpace, MaxLength(500)]
     public string? Photo { get; set; }
-
+    [NonSpace, MaxLength(600)]
     public string? Description { get; set; }
-
+    [NonSpace, MaxLength(10)]
     public string? NextPosition { get; set; }
 
     public int? Seniority { get; set; }
-
+    [NonSpace, MaxLength(30)]
     public string? Brithday { get; set; }
 }
---
+++ FTBAPI/Validations/NonSpaceAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FTBAPI.Validations
+{
+    public class NonSpaceAttribute:ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value?.ToString()?.Contains(" ") == true)
+            {
+                return new ValidationResult("該字串不可包含空格!!!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
